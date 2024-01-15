6387a0063c4ca0662c1c6ea3dc798b0972269df0
Ramsay <ramsayveitch123@gmail.com>
2024-01-15 19:57:06 +0000

    slight tweaks for frontend

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,14 @@
-import express from "express"
-import {getFeedPosts, getUserPosts, likePost} from "../controllers/posts.js"
-import { verifyToken } from "../middleware/auth.js"
+import express from "express";
+import { getFeedPosts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
 
-const router = express.Router()
+const router = express.Router();
 
-// READ
+/* READ */
 router.get("/", verifyToken, getFeedPosts);
-router.get("/:userId/posts")
+router.get("/:userId/posts", verifyToken, getUserPosts);
 
-// UPDATE
-
+/* UPDATE */
 router.patch("/:id/like", verifyToken, likePost);
 
-export default router
+export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,18 @@
-import express from "express"
+import express from "express";
 import {
-    getUser,
-    getUserFriends,
-    addRemoveFriend,
-} from "../controllers/users.js"
-import { verifyToken} from "../middleware/auth.js"
-import pkg from 'jsonwebtoken'
+  getUser,
+  getUserFriends,
+  addRemoveFriend,
+} from "../controllers/users.js";
+import { verifyToken } from "../middleware/auth.js";
 
+const router = express.Router();
 
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
 
-const router = express.Router()
-const { verify } = pkg
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
 
-// READ
-router.get("/:id", verifyToken, getUser)
-router.get("/:id/friends", verifyToken, getUserFriends)
-
-// UPDATE
-router.patch("/:id/:friendId", verifyToken)
-
-
-export default router
+export default router;
